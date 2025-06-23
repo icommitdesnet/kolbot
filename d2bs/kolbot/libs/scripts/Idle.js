2ecc536be8b22591be5e986ef3a5a438d74ffac6
theBGuy <60308670+theBGuy@users.noreply.github.com>
2025-06-23 01:19:16 -0400

    feat: Update Idle script to move every 10-12 in combination with the refresh packet
    - Noticed occasionally the idler's were still being disconnected so this is enough to stop that

--- d2bs/kolbot/libs/scripts/Idle.js
+++ d2bs/kolbot/libs/scripts/Idle.js
@@ -5,60 +5,96 @@
 *
 */
 
-const Idle = new Runnable(
-  function Idle () {
-    const greet = [];
-    // eslint-disable-next-line no-unused-vars
-    function gameEvent (mode, param1, param2, name1, name2) {
-      switch (mode) {
-      case 0x02:
-        // idle in town
-        me.inTown && me.mode === sdk.player.mode.StandingInTown && greet.push(name1);
+/**
+ * @typedef {ScriptContext & { cleanup: () => void }} IdleContext
+ */
 
+const Idle = new Runnable(
+  /**
+   * @param {IdleContext} ctx
+   */
+  function Idle (ctx) {
+    const greet = [];
+
+    /**
+     * @param {number} mode 
+     * @param {number} param1 
+     * @param {number} param2 
+     * @param {string} name1 
+     * @param {string} name2 
+     */
+    function gameEvent (mode, param1, param2, name1, _name2) {
+      switch (mode) {
+      case 0x02: // "%Name1(%Name2) joined our world. Diablo's minions grow stronger."
+        // idle in town
+        if (me.inTown && me.mode === sdk.player.mode.StandingInTown) {
+          greet.push(name1);
+        }
         break;
       }
     }
 
-    try {
-      const startTime = getTickCount();
-      let idleTick = getTickCount() + Time.seconds(rand(1200, 1500));
-      if (Config.Idle.Advertise) {
-        addEventListener("gameevent", gameEvent);
+    ctx.cleanup = function () {
+      removeEventListener("gameevent", gameEvent);
+    };
+    
+    const startTime = getTickCount();
+    // send anti-idle packet every ~20 minutes
+    let idleTick = getTickCount() + Time.seconds(rand(1200, 1500));
+    // move to waypoint/stash every 10-12 minutes
+    let moveTick = getTickCount() + Time.seconds(rand(600, 720));
+
+    if (Config.Idle.Advertise) {
+      addEventListener("gameevent", gameEvent);
+    }
+
+    while (true) {
+      if (!me.inArea(sdk.areas.RogueEncampment)) {
+        Town.goToTown(1);
+      } else if (Town.getDistance("stash") > 10) {
+        Town.move("stash");
       }
 
-      while (true) {
-        if (!me.inArea(sdk.areas.RogueEncampment)) {
-          Town.goToTown(1);
-        } else if (Town.getDistance("stash") > 10) {
+      if (Config.Idle.MaxGameLength > 0
+        && getTickCount() - startTime > Time.minutes(Config.Idle.MaxGameLength)) {
+        break;
+      }
+
+      if (Config.Idle.Advertise) {
+        while (greet.length) {
+          let name = greet.shift();
+          say("!Welcome " + name + ". " + Config.Idle.AdvertiseMessage);
+        }
+      }
+
+      if (getTickCount() - idleTick > 0) {
+        Packet.questRefresh();
+        idleTick += Time.seconds(rand(1200, 1500));
+        console.log("Sent anti-idle packet, next refresh in: (" + Time.format(idleTick - getTickCount()) + ")");
+      }
+
+      if (getTickCount() - moveTick > 0) {
+        if (me.inTown && me.mode === sdk.player.mode.StandingInTown) {
+          Town.move("waypoint");
           Town.move("stash");
         }
-        if (Config.Idle.MaxGameLength > 0
-          && getTickCount() - startTime > Time.minutes(Config.Idle.MaxGameLength)) {
-          break;
-        }
-        if (Config.Idle.Advertise) {
-          while (greet.length) {
-            let name = greet.shift();
-            say("!Welcome " + name + ". " + Config.Idle.AdvertiseMessage);
-          }
-        }
-        if (getTickCount() - idleTick > 0) {
-          Packet.questRefresh();
-          idleTick += Time.seconds(rand(1200, 1500));
-          console.log("Sent anti-idle packet, next refresh in: (" + Time.format(idleTick - getTickCount()) + ")");
-        }
-
-        delay(1000);
+        moveTick += Time.seconds(rand(600, 720));
+        console.log("Moved to waypoint/stash, next move in: (" + Time.format(moveTick - getTickCount()) + ")");
       }
 
-      return true;
-    } finally {
-      // cleanup
-      removeEventListener("gameevent", gameEvent);
+      delay(1000);
     }
+
+    return true;
   },
   {
     startArea: sdk.areas.RogueEncampment,
-    preAction: null
+    preAction: null,
+    /**
+     * @param {IdleContext} ctx
+     */
+    cleanup: function (ctx) {
+      ctx.cleanup();
+    }
   }
 );
